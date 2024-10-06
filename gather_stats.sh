eb9d26b438238ef9b779fef58bda9086f7468aca
Michael Rothgang <rothgang@math.uni-bonn.de>
2024-10-06 23:33:13 +0200

    chore: surface errors in gather_stats in CI

--- gather_stats.sh
+++ gather_stats.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Surface errors in this script to CI, so they get noticed.
+# See e.g. http://redsymbol.net/articles/unofficial-bash-strict-mode/ for explanation.
+set -e -u -o pipefail
+
 TIMEDELTA=$1
 
 # Change to the directory where the script is located
