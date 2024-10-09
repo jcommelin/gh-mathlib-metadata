7aeba59a1c2d4135b06fe2e8863cb0d4a5e54ff0
Michael Rothgang <rothgang@math.uni-bonn.de>
2024-10-10 00:16:25 +0200

    feat: add a script to produce aggregate data on PRs:
    for each PR, collect their draft status, whether CI passes and their base branch.
    The queueboard repository will be able to use this.

---
+++ process.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+'''
+This script looks at all files in the `data` directory and creates a JSON file,
+containing information about all PRs described in that directory. For each PR,
+we list
+- whether it's in draft stage (as opposed being marked as "ready for review")
+- whether mathlib's CI passes on it
+- the branch it is based on (usually "master")
+'''
+
+import json
+import os
+from datetime import datetime, timezone
+from typing import List
+
+
+# 'items' is a list of timeline items for this pull request
+def is_in_draft_state(items: dict) -> bool:
+    # xxx what's the default here? is there always an explicit event?
+    is_draft = False
+    events_data = items["nodes"]
+    for event in events_data:
+        type = event["__typename"]
+        if type == "ReadyForReviewEvent":
+            is_draft = False
+        elif type == "ConvertToDraftEvent":
+            is_draft = True
+    return is_draft
+
+
+def main():
+    output = dict()
+    updated = datetime.now(timezone.utc).strftime("%B %d, %Y at %H:%M UTC")
+    output["timestamp"] = updated
+    pr_data = []
+    # Read all pr info files in the data directory.
+    pr_names : List[str] = sorted(os.listdir("data"))
+    for pr_number in pr_names:
+        with open(f"data/{pr_number}/pr_info.json", "r") as fi:
+            data = json.load(fi)
+            inner = data["data"]["repository"]["pullRequest"]
+            number = inner["number"]
+            base_branch = inner["baseRefName"]
+            is_draft = is_in_draft_state(inner["timelineItems"])
+
+            CI_passes = False
+            # Get information about the latest CI run. We just look at the "summary job".
+            CI_runs = inner["statusCheckRollup"]["contexts"]["nodes"]
+            for r in CI_runs:
+                # Ignore bors runs: these don't have a job name (and are not interesting for us).
+                if "context" in r:
+                    pass
+                elif r["name"] == "Summary":
+                    CI_passes = True if r["conclusion"] == "SUCCESS" else False
+            d = {
+                "number": number,
+                "is_draft": is_draft,
+                "CI_passes": CI_passes,
+                "base_branch" : base_branch
+            }
+            pr_data.append(d)
+    output["pr_statusses"] = pr_data
+    with open("processed_data/aggregate_pr_data.json", "w") as f:
+        print(json.dumps(output, indent=4), file=f)
+
+main()
